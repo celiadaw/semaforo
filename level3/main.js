5082a483da5df2ab1fe8968db734f4d52bae81d5
celiadaw <celiasalamancag@gmail.com>
2021-02-22 09:00:11 +0100

    cambios del viernes en clase

--- level3/main.js
+++ level3/main.js
@@ -9,9 +9,9 @@ const buttonCreate = document.getElementById("buttonCreate");
 
 
 // REVISAR!!!!!!NO FUNCIONA
-// let numberOfColors = document.getElementById("numberOfColors").value; 
-numberOfColors=3;
-console.log(numberOfColors);//no muestra nada
+// let numberOfColors = document.getElementById("numberOfColors").value; //meter en la funcion crear!
+// numberOfColors=3;
+// console.log(numberOfColors);//no muestra nada
 //estamos recogiendo la etiqueta numberOfcolors...
 /*
 DONDE RECOGEMOS EL VALOR QUE NOS DAN POR PANTALLA?'
@@ -47,36 +47,51 @@ function getColorRandom() {//funcion que elige un color random
 
     return  Math.floor(Math.random() * (256 - 0)) + 0; 
 }
- function enableColor(position) {//funcion que enciende
-    console.log(position + " esta es la bandera.. la posicion para recoger los elementos que quiero")
-    console.log("estoy intentando ver el elemento circle de la posicion" +position+ " es  "+ color[position].circle+ " y el color de ese objeto es " + color[position].color);
-    colors[position].
-      colors[position].circle.style.backgroundColor(colors[position].color);
+ function enableColor(flag) {//funcion que enciende
+    let newColor=RGB();
+  
+    //document.getElementById("id"+position)("style", `backgroundColor:(newColor`);
+   let luzQueCambio=document.getAttribute("id"+flag);
+  console.log("este es el elemento que quiero ver si cojo bien "+luzQueCambio);
+    luzQueCambio.classList.remove("black");
+
+    colors[positions[]].setAttribute("style",`background-color:${newColor}`);
+    
+     
+    
+   // document.getElementById("id"+position).style.backgroundColor = newColor;
+    
 
 }
 
-function disableColor(position) {//funcion que apaga
-  
-    colors[position].circle.style.backgroundColor("rgb(0,0,0)");
+function disableColor(flag) {//funcion que apaga
+    document.getElementById("id"+flag).classList.add("black");
+    // colors[position].circle.style.backgroundColor("rgb(0,0,0)");
 }
 
 
 function changeColor() {
-    console.log("flag al empezar changeColor" + flag);
-    flag=1;
-    console.log("flag despues de asignar 1 en changeColor" + flag);
+ 
+let colorsLength=colors.length;
+console.log(colorsLength);
+// console.log(colors[0].getElementById(`id${flag}`));
+console.log(colors[1]);
+
+
     enableColor(flag);
+console.log("flag es "+flag);
    if(flag==0){
 
-       disableColor(colors.length-1);
-             
-   } else if(flag==colors.length-1){
+       disableColor(colorsLength-1);
+       flag++;
+   } else if(flag==colorsLength-1){
       
             flag==0;
      }else{
         disableColor(flag-1);
        flag++;
      }
+  
 }
 
 
@@ -102,27 +117,33 @@ console.log(RGB());
 //vamos añadiendo colores al array Y AÑADIMOS NUEVOS CAMPOS
 function createColors () {
 let newCircle;
+let numberOfColors = document.getElementById("numberOfColors").value;
+let positions = [];
 for (let i=0; i<numberOfColors;i++){
-    let newColor=RGB();
-    console.log(newColor);
-   //  let newClass = "circle"+i;
     let newId= "id"+i;
-    
+   //  let newClass = "circle"+i;
+   
+let numberFlag = newId.slice(-1);
+    console.log("numberFlag"+numberFlag);
     //debemos crear un nuevo div, (circule con clase button y despues añadirle la clase background color con newColor)
 
     newCircle = document.createElement("div");
-    console.log(newCircle);
-    newCircle.setAttribute("class", "button");
-    newCircle.setAttribute("id", newId);
+    console.log("creando nuevo circlo  "+newCircle);
+    newCircle.setAttribute("class", "button black");
+     newCircle.setAttribute("id", `${newId}`);
+    // newCircle.setAttribute("data-id"=`${newId}`);
+    // newCircle.dataset("id" , `${newId}`);
+    // newCircle.nodeValue("id" , `${newId}`);
+    // console.log(document.getElementById(`${newId}`).getAttribute("data-value"));
     //añadimos container
     container.appendChild(newCircle);
+    // document.getElementById(newId).("style", `backgroundColor:(newColor)`);
    //  newCircle.style.backgroundColor(newColor);
 
-       colors[i]={
-        circle: newCircle, 
-        color: newColor};
-    console.log("posicion 0 del array y lo que hay dentro del objeto " + colors[0].circle +"  2 cosa del objeto" + colors[0].color);
-    
+       colors[i]=newCircle; 
+       positions[i]=numberFlag;
+       console.log("posición del array "+i+ "con e elemento"+newId);
+   
    
 }
 
